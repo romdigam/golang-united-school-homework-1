cc7bc51ab62ae79393ab6b7ddfbab6232bff380a
rom <digam19@gmail.com>
2022-02-14 09:27:58 +0300

    1 solution

--- solution.go
+++ solution.go
@@ -1,5 +1,16 @@
 package solution
 
+import (
+    "github.com/kyokomi/emoji/v2"
+)
+
+
 func GetMessage() string {
-	return ""
+    hello := "Hello"
+    world := " 🗺️ "
+    rendered := emoji.Sprint(hello, world)
+    rendered = rendered + "!"
+    return rendered
+
 }
+
